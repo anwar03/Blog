f6fa696ac8ed95892861881461a56fad9fcd1ad5
anwar03 <anwarazim03@gmail.com>
2018-02-17 01:17:20 +0600

    Added login API.

--- project-base/profiles/urls.py
+++ project-base/profiles/urls.py
@@ -7,6 +7,7 @@ from . import views
 router = DefaultRouter()
 
 router.register('user-profile', views.UserProfileViewSet, base_name='user-profile')
+router.register('login', views.LoginViewSet, base_name='login')
 
 urlpatterns = [
     #url(r'^user-profile/$', views.UserProfileViewSet, name='user-profile'),
--- project-base/profiles/views.py
+++ project-base/profiles/views.py
@@ -6,7 +6,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
-
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
 
 from . import serializers
 from .models import User
@@ -22,3 +23,15 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (UserUpdatePermission, )
     filter_backends = (filters.SearchFilter,)
     search_fields = ('email', 'first_name',)
+
+
+class LoginViewSet(viewsets.ViewSet):
+    """Checks user email and password return auth token."""
+
+    serializer_class = AuthTokenSerializer
+
+    def create(self, request):
+        """use the obtainAuthToken ApiView to validate and create a token."""
+
+        return ObtainAuthToken().post(request)
+
