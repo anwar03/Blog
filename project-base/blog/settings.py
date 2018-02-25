185e51868438e619e9657defef9e8fe528177492
anwar03 <anwarazim03@gmail.com>
2018-02-25 22:01:29 +0600

    Rename project name.

--- project-base/blog/settings.py
+++ project-base/blog/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'instagram.urls'
+ROOT_URLCONF = 'blog.urls'
 
 TEMPLATES = [
     {
@@ -75,7 +75,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'instagram.wsgi.application'
+WSGI_APPLICATION = 'blog.wsgi.application'
 
 
 # Database
